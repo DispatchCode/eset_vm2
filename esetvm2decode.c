0a5817e5f6ee95c1e7061e40d298a8a27cafd4aa
Marco Crivellari <crivellari.marco90@gmail.com>
2024-11-03 23:24:39 +0100

    Refactoring, improvements, fix

--- esetvm2decode.c
+++ esetvm2decode.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "esetvm2hdr.h"
 #include "esetvm2.h"
@@ -10,16 +11,16 @@
 #ifdef DEBUG_PRINT_INSTR
 static void print_decoded_instr(struct esetvm2_instruction instr) {
 	struct instr_info info = instr_table[instr.op_table_index];
-	
+
 	printf("%s \t", info.mnemonic);
-	
+
 	if(info.constant)
 		printf("%ld", instr.constant);
 
 	if(info.addr) {
 		// TODO print address
 	}	
-	
+
 	if(info.nr_args) printf(", ");
 
 	for(int i=0; i<info.nr_args; i++) {
@@ -33,15 +34,15 @@ static void print_decoded_instr(struct esetvm2_instruction instr) {
 #ifdef DEBUG_INSTR_FORMAT
 static void print_internal_rapresentation(struct instr_info info) {
 	printf("%s(%x) \t", info.mnemonic, opcode_map[info.op_table_index]);
-	
+
 	if (info.constant)
 		printf("constant");
-	
+
 	if (info.addr)
 		printf("address");
 
 	if (info.nr_args) printf(", ");
-	
+
 	for(int i=0; i<info.nr_args; i++) 
 	{
 		printf("arg%d%c", i+1, (i != info.nr_args-1) ? ',' : ' ');
@@ -59,7 +60,7 @@ static inline int get_bit_at(struct esetvm2 *vm, int pos)
 	return (vm->memory[byte_index] >> (7 - bit_index) & 1);
 }
 
-static int64_t read_const(struct esetvm2 *vm, int start, int num_bits) {
+static inline int64_t read_const(struct esetvm2 *vm, int start, int num_bits) {
 	int64_t constant = 0;
 	for(int i=0; i < num_bits; i++) {
 		constant = (constant << 1) | get_bit_at(vm, start + (num_bits - 1) - i);
@@ -67,13 +68,17 @@ static int64_t read_const(struct esetvm2 *vm, int start, int num_bits) {
 	return constant;
 }
 
-static struct esetvm2_instruction decode_instruction(struct esetvm2 *vm, struct instr_info info) {
+static struct esetvm2_instruction decode_instruction(struct esetvm2 *vm, int op_map_index, int code_offset) {
 	struct esetvm2_instruction instr;
+	struct instr_info info;
 	int code_bit;
 
-	instr.op_table_index = info.op_table_index;
-	code_bit = CODE_OFFSET_BIT + info.code_offset + info.op_size;
-	
+	info = instr_table[op_map_index];
+	instr.op_table_index = op_map_index;
+
+	// TODO stored inside the VM ?
+	code_bit = CODE_OFFSET_BIT + code_offset + info.op_size;
+
 	if (info.constant) {
 		//printf("code_offset: %d, op size: %d\n", info.code_offset, info.op_size);
 		instr.constant = read_const(vm, code_bit, 64);
@@ -84,23 +89,29 @@ static struct esetvm2_instruction decode_instruction(struct esetvm2 *vm, struct 
 		// TODO address parsing
 	}
 
-	if(info.nr_args) {
-		for(int i=0; i < info.nr_args; i++) {
-			int arg;
-			switch(get_bit_at(vm, code_bit)) {
-				case 0:
-					arg = read_const(vm, code_bit+1, 4);	
-				case 1:
+	for(int i=0; i < info.nr_args; i++) {
+		switch(get_bit_at(vm, code_bit)) {
+			case 0:
+				instr.arg[i] = read_const(vm, code_bit+1, 4);
+				code_bit += 5;
+				break;	
+			case 1:
 				// TODO argx, ss, xxxx
-			}
-
-			instr.arg[i] = arg;
-			//printf("Register nr: %d\n", arg);
+				break;
 		}
 	}
 
+	instr.len = info.len;
+
 	return instr;
 }
+
+static inline int get_op_map_index(uint8_t opcode) {
+	int i = -1;
+	while(++i < sizeof(opcode_map) && opcode != opcode_map[i]);
+	return i;
+}
+
 
 struct esetvm2_instruction *decode(struct esetvm2hdr *hdr, struct esetvm2 *vm) {
 	// TODO use the correct CODE size
@@ -109,63 +120,64 @@ struct esetvm2_instruction *decode(struct esetvm2hdr *hdr, struct esetvm2 *vm) {
 	int buff_shift = 0;
 	int instr = 0;
 	int tot_instr = hdr->code_size + CODE_OFFSET;
-   	int code_off = 0;
- 
+	int code_off = 0;
+
+	int stop_after = 0;
 	while(buff_index < tot_instr)
 	{
 		uint8_t tmp;
+		struct esetvm2_instruction instr_decoded;
 
 		// the opcode is inside the first byte
 		if (buff_shift < 4) {
 			tmp = vm->memory[buff_index] << buff_shift;
-        }
+		}
 		// OP is partially in the first byte, so we need bits from the next byte
-        else if (buff_shift < 8 && buff_shift > 3) {
-            tmp = (vm->memory[buff_index] << buff_shift) | (vm->memory[buff_index+1] >> (8 - buff_shift));
-        }
+		else if (buff_shift < 8 && buff_shift > 3) {
+			tmp = (vm->memory[buff_index] << buff_shift) | (vm->memory[buff_index+1] >> (8 - buff_shift));
+		}
 		// remainder >=8, so we can discard the byte and take the next
-        else {
-            buff_shift %= 8;
-            tmp = vm->memory[++buff_index] << buff_shift;
-        }
+		else {
+			buff_shift %= 8;
+			tmp = vm->memory[++buff_index] << buff_shift;
+		}
 
-        // get the "group" from the opcode
-        uint8_t grp = (tmp & 0xE0) >> 5;
-        // mask of the group (used to retrieve the index)
-        uint8_t grp_mask = op_grp_table[grp];
-        // the "index" part of the opcode
-        uint8_t grp_index = (tmp & grp_mask) >> op_index_shift[grp];
+		// get the "group" from the opcode
+		uint8_t grp = (tmp & 0xE0) >> 5;
+		// mask of the group (used to retrieve the index)
+		uint8_t grp_mask = op_grp_table[grp];
+		// the "index" part of the opcode
+		uint8_t grp_index = (tmp & grp_mask) >> op_index_shift[grp];
 
 		// byte aligned opcode, like in the opcode_map
-        uint8_t op = (grp << 5) | (grp_index << op_index_shift[grp]);
+		uint8_t op = (grp << 5) | (grp_index << op_index_shift[grp]);
 		//printf("opcode: %x, opcode map sizeof: %d\n", op, sizeof(opcode_map));
 
-        int i = -1;
-        while(++i < sizeof(opcode_map) && op != opcode_map[i]);
+		int op_map_index = get_op_map_index(op);
+
 		// TODO check out of bounds
 		//printf("Index OPcode Map: %d\n", i);
-		// Internal rappresentation of an instruction,
-		// used to describe the format
-        struct instr_info info = instr_table[i];
+
 #ifdef DEBUG_INSTR_FORMAT
-		print_internal_rapresentation(info);
+		print_internal_rapresentation(instr_table[op_map_index]);
 #endif
 
-		info.memory_index = buff_index;
-		info.op_table_index = i;
-		info.code_offset = code_off;		
-		code_off += info.len;
+		instr_decoded = decode_instruction(vm, op_map_index, code_off);
 
-		instructions[instr++] = decode_instruction(vm, info);
 #ifdef DEBUG_PRINT_INSTR
-		print_decoded_instr(instructions[instr-1]);
+		print_decoded_instr(instr_decoded);
 #endif		
+		instructions[instr++] = instr_decoded;
 
-        buff_index += (info.len >> 3);
-        buff_shift += (info.len % 8);
-		
-		break;
+		buff_index += (instr_decoded.len >> 3);
+		buff_shift += (instr_decoded.len % 8);
+
+		code_off += instr_decoded.len;
+
+		stop_after++;
+		if(stop_after == 2)
+			break;
 	}
 
-    return instructions;
+	return instructions;
 }
